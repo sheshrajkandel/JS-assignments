506060c51ab529d9879e44337fa4cc97411fb42e
Sheshraj Kandel <sheshrajkandel@gmail.com>
2019-10-01 04:18:01 +0545

    first day assignments

---
+++ pattern.js
@@ -0,0 +1,8 @@
+for (i = 0; i < 5; i++) {
+  var txt = "";
+  for (j = 0; j < 5 - i; j++) {
+    txt += "* ";
+  }
+  console.log(txt);
+  console.log();
+}
---
+++ profile.js
@@ -0,0 +1,10 @@
+var profile = {
+  name: "Sheshraj Kandel",
+  edu: ["Bachelor of Computer Applications", "LPU", "2019"],
+  address: "Nawalparasi",
+  country: "Nepal",
+  email: "sheshrajkandel@gmail.com",
+  mobile: 9847274072,
+  dob: 1993
+};
+console.log(profile);
